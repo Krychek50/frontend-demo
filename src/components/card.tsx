187853bc29277a257d9fd5bb196e9f4bde1a7b83
Tom <tsnuttall@gmail.com>
2022-09-21 21:39:27 +0100

    Initial basic Reactapp with json api request and display

---
+++ src/components/card.tsx
@@ -0,0 +1,19 @@
+export interface ICardData {
+  name: string,
+  username: string,
+  email: string
+}
+
+interface ICardProps {
+	card: ICardData;
+}
+
+export function Card(props: ICardProps) {
+	return (
+		<div>
+			<div>{props.card.name}</div>
+      <div>{props.card.username}</div>
+      <div>{props.card.email}</div>
+		</div>
+	);
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash:8].js',
+    //sourceMapFilename: '[name].[hash:8].map',
+    //chunkFilename: '[id].[hash:8].js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Demo',
+      template: './template/index.html'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css',
+    })
+  ],
+  devServer: {
+	  static: path.resolve(__dirname, 'dist'),
+  }
+};
